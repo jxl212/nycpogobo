ab1e1c35ec02b2b553b1c80df1dada51cf4529e4
Jeremy LeCroy <jeremy212@gmail.com>
2018-01-14 05:07:32 -0500

    load_config connects to db alone

--- config.py
+++ config.py
@@ -1,12 +1,18 @@
 from configparser import ConfigParser
-from utils import db
+import os, re
+from pymongo import MongoClient
 
 def load_config():
     parser=ConfigParser()
+    mongodb_user=os.environ.get("MONGO_USER")
+    mongodb_pass=os.environ.get("MONGO_PASS")
+
+    mongo_client = MongoClient("mongodb+srv://{}:{}@cluster0-m6kv9.mongodb.net/nyc".format(mongodb_user,mongodb_pass))
+    db = mongo_client.nyc
+
     ret=db.config.find_one({ "_id": {"$exists":True} },{"_id":False})
     parser.read_dict({"DEFAULT":ret})
     config = list(parser['DEFAULT'].items())
     return config
 
 config = load_config()
- 
