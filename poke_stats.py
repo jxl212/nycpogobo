8064dff7acc46cc7892d2cf4365c4119c1f72551
Jeremy LeCroy <jeremy212@gmail.com>
2018-01-14 00:27:34 -0500

    fix bad merge

--- poke_stats.py
+++ poke_stats.py
@@ -18,16 +18,8 @@ class PokeStats:
         first=self.pokemons[pokemon_name].queue[0]
         last=self.pokemons[pokemon_name].queue[-1]
         delta=last-first
-<<<<<<< HEAD
         if delta.total_seconds() == 0:
             return 0, 0, 0
-=======
-<<<<<<< HEAD
-        if delta.total_seconds() == 0:
-            return None, None, None
-=======
->>>>>>> b9750c6b04b6a8fdef6548fbc6eb098675e0dfee
->>>>>>> 067a7f77ecf989d7f87a43c8a94aef6ca39f9677
         per_second=int(size) / delta.total_seconds()
         per_minute=int(size) / delta.total_seconds() / 60
         per_hour=int(size) / delta.total_seconds() / 3660
